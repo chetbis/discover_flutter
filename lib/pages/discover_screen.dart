f77500b287df1dd4790fdbd7f4cb4de1953ea4eb
chetbis <chetiabishnu95@gmail.com>
2023-09-04 13:19:57 +0530

    adjust padding

--- lib/pages/discover_screen.dart
+++ lib/pages/discover_screen.dart
@@ -11,7 +11,6 @@ class DiscoverScreen extends StatefulWidget {
 
 class _DiscoverScreenState extends State<DiscoverScreen>
     with SingleTickerProviderStateMixin {
-
   late TabController _tabController;
 
   final List<String> _tabs = ['All', 'Birds', 'Fishes', 'Mammals'];
@@ -27,82 +26,84 @@ class _DiscoverScreenState extends State<DiscoverScreen>
 
   @override
   Widget build(BuildContext context) {
-    final query = MediaQuery.of(context);
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.fromLTRB(
-          20,
-          query.padding.top,
-          20,
-          query.padding.bottom,
-        ),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color(0xff524a41),
-              Color(0xffaa928e),
-            ],
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.only(
+            top: 10,
+            left: 20,
+            right: 20,
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(
-                  Icons.menu,
-                  size: 30,
-                ),
-                Icon(
-                  Icons.search,
-                  size: 30,
-                )
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xff524a41),
+                Color(0xffaa928e),
               ],
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Discover',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            const Text(
-              'Protect our Earth together.',
-              style: TextStyle(
-                fontSize: 15,
-                height: 1.5,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TabBar(
-              automaticIndicatorColorAdjustment: false,
-              labelPadding: EdgeInsets.zero,
-              indicator: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Colors.amber,
-              ),
-              controller: _tabController,
-              tabs: _tabs.map((e) => Tab(text: e)).toList(),
-            ),
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  AnimalList(list: birdData),
-                  AnimalList(list: mammalData),
-                  AnimalList(list: fishData),
-                  AnimalList(list: fishData),
+                  Icon(
+                    Icons.menu,
+                    size: 30,
+                  ),
+                  Icon(
+                    Icons.search,
+                    size: 30,
+                  )
                 ],
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              const Text(
+                'Discover',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const Text(
+                'Protect our Earth together.',
+                style: TextStyle(
+                  fontSize: 15,
+                  height: 1.5,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TabBar(
+                automaticIndicatorColorAdjustment: false,
+                labelPadding: EdgeInsets.zero,
+                indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.amber,
+                ),
+                controller: _tabController,
+                tabs: _tabs.map((e) => Tab(text: e)).toList(),
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    AnimalList(
+                      list: [...birdData, ...mammalData, ...fishData],
+                    ),
+                    AnimalList(list: mammalData),
+                    AnimalList(list: fishData),
+                    AnimalList(list: fishData),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
